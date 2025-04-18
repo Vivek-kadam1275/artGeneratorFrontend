f468c52ad96df4600d9ae92106acd920576311b2
Vivek Kadam <vk2567298@gmail.com>
2025-04-19 00:16:09 +0530

    created separate card for images and added text for no data found

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import Navbar from "../components/Navbar.jsx";
 import { getImages } from "../utils/ApiRoutes.jsx";
 import { useLocation } from "react-router-dom";
+import ImageCard from "../components/ImageCard.jsx";
 const Gallery = (props) => {
 
   const [imagesData, setImagesData] = useState([]);
@@ -19,47 +20,41 @@ const Gallery = (props) => {
   }
   useEffect(() => {
     getImagesData();
+    
   }, [location.pathname]);
 
 
-  const downloadImage = async (url, prompt = "generated-art") => {
-    try {
-      const response = await fetch(url, { mode: "cors" });
-      const blob = await response.blob();
-      const blobUrl = window.URL.createObjectURL(blob);
+   //   try {
+  //     const response = await fetch(url, { mode: "cors" });
+  //     const blob = await response.blob();
+  //     const blobUrl = window.URL.createObjectURL(blob);
 
-      const link = document.createElement("a");
-      link.href = blobUrl;
-      link.download = `${prompt.replace(/\s+/g, "-")}.png`; // dynamic filename
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    } catch (error) {
-      console.error("Download failed:", error);
-    }
-  };
+  //     const link = document.createElement("a");
+  //     link.href = blobUrl;
+  //     link.download = `${prompt.replace(/\s+/g, "-")}.png`; // dynamic filename
+  //     document.body.appendChild(link);
+  //     link.click();
+  //     document.body.removeChild(link);
+  //   } catch (error) {
+  //     console.error("Download failed:", error);
+  //   }
+  // };
   return (
     <div className="h-screen">
       <Navbar />
       <div className="bg-gradient-to-b from-purple-600 to-blue-900 pt-10 md:min-h-[90%] ">
-        {loading ? <div className="w-full h-80 ">
-          loading
-        </div> : <div className=" w-[70%] mx-auto flex flex-wrap justify-center gap-8  ">
+        {loading ? <div className="w-full h-80 flex justify-center items-center ">
+          <div class="spinner"></div>
+        </div> : <div >
+          {imagesData.length>0 ? <div className=" w-[70%] mx-auto flex flex-wrap justify-center gap-8  ">
           {imagesData.map((item) => {
-            return <div key={item._id} className="  flex flex-col items-center bg-white/10 backdrop-blur-md border border-white/20 p-4 rounded-2xl shadow-lg hover:scale-105 transition-transform duration-300 w-[320px]">
-              <div className=" text-white text-lg font-semibold text-center mb-3 drop-shadow">{item.prompt}</div>
-              <img src={item.imageUrl} alt="image" className="mx-auto w-[300px] h-[300px] object-cover rounded-xl" />
-
-              <button
-                onClick={() => downloadImage(item.imageUrl, item.prompt)}
-                className="mt-4 bg-green-600 text-white py-2 px-4 rounded hover:scale-105"
-              >
-                Download Image
-              </button>
-
-            </div>
+            return  <ImageCard key={item._id} url={item.imageUrl} prompt={item.prompt}/>
           })}
-        </div>}
+        </div>:<div className=" w-[70%] md:min-h-50 justify-center flex items-center mx-auto text-2xl text-center  text-white ">
+             No artwork found...
+          </div>}
+        </div>
+        }
       </div>
     </div>
   )
